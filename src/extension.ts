0d4d82078ff129b93d78ff59b4f23ef066e6ef37
Antonio Barcelos <antonio.barcelos@neo4j.com>
2021-06-09 15:50:34 +0200

    Run custom queries

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,43 @@
 import * as vscode from 'vscode';
 import neo4j, { Driver } from 'neo4j-driver';
 
-let driver: Driver ; 
+let driver: Driver;
 
 export function activate(context: vscode.ExtensionContext) {
-	driver = neo4j.driver('neo4j://localhost', neo4j.auth.basic('neo4j', 'pass'));
+	driver = neo4j.driver('neo4j://localhost', neo4j.auth.basic('neo4j', 'pass'), {
+		useBigInt: true
+	});
 
 	console.log('Congratulations, your extension "neo4j-cypher-runner" is now active!');
 
 	let disposable = vscode.commands.registerCommand('neo4j-cypher-runner.run-query', async () => {
-		const session = driver.session();
-		try {
-			const json: string = await session.writeTransaction(async tx => {
-				const result = await tx.run('RETURN 1 AS n');
-				return JSON.stringify(result.records.map(x => x.toObject()));
-			});
+		const document = vscode.window.activeTextEditor?.document;
+		if (document && document?.fileName.endsWith('.cypher')) {
+			const query = document.getText();
+			const session = driver.session();
+			try {
+				const json: string = await session.writeTransaction(async tx => {
+					const result = await tx.run(query);
+					return JSON.stringify(
+						result.records.map(x => x.toObject()),
+						(_, value) => typeof value === 'bigint' ? `${value}n` : value,
+						4
+					);
+				});
 
-			vscode.window.showInformationMessage(json);
+				const resultDocument = await vscode.workspace.openTextDocument({
+					language: 'json',
+					content: json
+				});
 
-		} finally {
-			session.close();
+				vscode.window.showTextDocument(resultDocument, 2, true);
+
+			} finally {
+				session.close();
+			}
+
 		}
-		
+
 	});
 
 	context.subscriptions.push(disposable);
