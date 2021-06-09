a2004bf0476e1ce79666b9e71b9354966eac4aa7
Florent Biville <florent.biville@neo4j.com>
2021-06-09 18:32:21 +0200

    Introduce query delimiters

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,35 @@ import neo4j, { Driver } from 'neo4j-driver';
 
 let driver: Driver;
 
+function previousDelimiter(editor: vscode.TextEditor): vscode.Position  {
+	const currentPosition = editor.selection.active
+	for (let i = currentPosition.line; i >= 0; i--) {
+		const line = editor.document.lineAt(i)
+		if (line.text.startsWith('####')) {
+			console.log(`Section starts at (${i+1},0)`)
+			return new vscode.Position(i + 1, 0)
+		}
+		
+	}
+	console.log(`Section starts at (0,0)`)
+	return new vscode.Position(0, 0)
+}
+
+function nextDelimiter(editor: vscode.TextEditor): vscode.Position  {
+	const currentPosition = editor.selection.active
+	const document = editor.document
+	const lineCount = document.lineCount
+	for (let i = currentPosition.line + 1; i < lineCount; i++) {
+		const line = document.lineAt(i)
+		if (line.text.startsWith('####')) {
+			console.log(`Section ends at (${i},0)`)
+			return new vscode.Position(i, 0)
+		}
+	}
+	console.log(`Section ends at (${lineCount},0)`)
+	return new vscode.Position(lineCount, 0)
+}
+
 export function activate(context: vscode.ExtensionContext) {
 	driver = neo4j.driver('neo4j://localhost', neo4j.auth.basic('neo4j', 'pass'), {
 		useBigInt: true
@@ -11,9 +40,11 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "neo4j-cypher-runner" is now active!');
 
 	let disposable = vscode.commands.registerCommand('neo4j-cypher-runner.run-query', async () => {
-		const document = vscode.window.activeTextEditor?.document;
+		const maybeEditor = vscode.window.activeTextEditor;
+		const document = maybeEditor?.document;
 		if (document && document?.fileName.endsWith('.cypher')) {
-			const query = document.getText();
+			const textEditor = maybeEditor!
+			const query = document.getText(new vscode.Range(previousDelimiter(textEditor), nextDelimiter(textEditor)));
 			const session = driver.session();
 			try {
 				const json: string = await session.writeTransaction(async tx => {
