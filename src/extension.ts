641ac9a2877b029c481cdcc2c67bc44d70382ab1
Florent Biville <445792+fbiville@users.noreply.github.com>
2021-06-10 15:58:12 +0200

    Fix typo (#5)
    fix typo

--- src/extension.ts
+++ src/extension.ts
@@ -52,20 +52,20 @@ export async function activate(context: vscode.ExtensionContext) {
 	}
 
 	async function performEnvironmentSelection() {
-		const enviroments = getConfiguredEnvironments();
-		if (enviroments.length > 0) {
-			const pickedConfig = await vscode.window.showQuickPick(
-				enviroments.map(env => env.name),
-				{
-					canPickMany: false,
-					title: 'Select Neo4j Enviroment',
-				}
-			);
-			selectedEnvironment = enviroments.find(db => db.name === pickedConfig);
-			configureDriver();
-		} else {
+		const environments = getConfiguredEnvironments();
+		if (environments.length == 0) {
 			vscode.window.showErrorMessage('No environment configured');
+			return;
 		}
+		const pickedConfig = await vscode.window.showQuickPick(
+			environments.map(env => env.name),
+			{
+				canPickMany: false,
+				title: 'Select Neo4j Enviroment',
+			}
+		);
+		selectedEnvironment = environments.find(db => db.name === pickedConfig);
+		configureDriver();
 
 	}
 
